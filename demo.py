e7c239a3df9208392dd1816d70e0a8d7417a8599
Sahil Tambe <sahiltambe1996@gmail.com>
2024-06-08 00:15:43 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -15,15 +15,22 @@ def main():
     # Link to GitHub repository
     st.markdown("[GitHub Repository](https://github.com/sahiltambe/Advanced-Retrieval-Augmented-Generation-RAG-with-OpenAI)", unsafe_allow_html=True)
 
-    
+
     st.markdown("""
-    **Note: This is a demonstration of application. To use this application effectively for your specific use case, 
-    please ensure you have completed the necessary pre-requisites as outlined in the <a href="https://github.com/sahiltambe/Advanced-Retrieval-Augmented-Generation-RAG-with-OpenAI?tab=readme-ov-file#instructions" target="_blank">Instructions</a>.** """,unsafe_allow_html=True)
+    **<span style='color: yellow;'>Note</span>: This is a demonstration of application. To use this application effectively for your specific use case, 
+    please ensure you have completed the necessary pre-requisites as outlined in the <a href="https://github.com/sahiltambe/Advanced-Retrieval-Augmented-Generation-RAG-with-OpenAI?tab=readme-ov-file#instructions" target="_blank">Instructions</a>.**
+    
+    <div style='text-align: center;'> <span style='color: White;'>OR</span></div>
+                
+    **For a comprehensive overview of the application and its implementation, visit my detailed <a href="https://medium.com/@sahiltambe131813/building-an-advanced-retrieval-augmented-generation-rag-with-openai-7ebdad88dc5a" target="_blank">blog post</a>.** """,unsafe_allow_html=True)
+
 
     st.title("Your Advance Auto RAG Assistant")
     
     # Sidebar for adding documents
+    
     st.sidebar.title("Add Documents to Knowledge Base")
+    
     uploaded_file = st.sidebar.file_uploader("Choose a PDF file", type="pdf")
     if uploaded_file is not None:
         with open(uploaded_file.name, "wb") as f:
